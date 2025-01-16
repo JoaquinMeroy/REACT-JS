294d77eaea11ec1d9a8751b7f64403635fdc45ea
Joaquin A. Meroy <167620611+JoaquinMeroy@users.noreply.github.com>
2025-01-16 10:59:37 +0800

    header
    Added header component in the website

--- Juliana-portfolio/src/Header.jsx
+++ Juliana-portfolio/src/Header.jsx
@@ -1,4 +1,16 @@
 function Header() {
-  return <header></header>;
+  return(
+    <header className="flex justify-between">
+      <h1>Hello</h1>
+      <div className="nav-buttons-container">
+        <ul>
+          <li>button1</li>
+          <li>button2</li>
+          <li>button3</li>
+          <li>button4</li>
+        </ul>
+      </div>
+    </header>
+  );
 }
-export default Header;
+export default Header
