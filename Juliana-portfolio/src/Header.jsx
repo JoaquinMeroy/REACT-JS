9715a25253617a5fac9dc00573ee5e2d0bde3d40
Joaquin A. Meroy <167620611+JoaquinMeroy@users.noreply.github.com>
2025-01-17 06:56:44 +0800

    New Content
    Added hero-image
    Added new font
    Updated Heading Navigation Buttons

--- Juliana-portfolio/src/Header.jsx
+++ Juliana-portfolio/src/Header.jsx
@@ -1,16 +1,32 @@
 function Header() {
   return(
-    <header className="flex justify-between">
-      <h1>Hello</h1>
+    <div className="container mx-auto">
+    <header className="flex justify-between my-5">
+      <h1 className="logo-name">Juliana</h1>
       <div className="nav-buttons-container">
-        <ul>
-          <li>button1</li>
-          <li>button2</li>
-          <li>button3</li>
-          <li>button4</li>
+        <ul className="flex">
+          <li>About me</li>
+          <li>Process</li>
+          <li>Gallery</li>
+          <li>Testimonials</li>
         </ul>
       </div>
     </header>
+
+    <div className="flex flex-wrap">
+  <div className="w-full md:w-1/2 flex justify-center">
+    <div className="hero-img-container">
+      <img src="src/assets/images/hero-img.svg" alt="hero image" className="w-full h-auto"/>
+    </div>
+  </div>
+  <div className="w-full sm:w-1/2 flex justify-center items-center">
+    <div className="hero-img-container">
+      <h1 className="sub-heading">Capturing Life in Watercolor</h1>
+    </div>
+  </div>
+</div>
+    </div>
+    
   );
 }
 export default Header
