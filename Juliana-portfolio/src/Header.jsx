4ce2849e7d18ec2aeaa9adcf3d8c4b7d03cbc04e
Joaquin A. Meroy <167620611+JoaquinMeroy@users.noreply.github.com>
2025-02-04 00:54:21 +0800

    Remove
    Remove the existing parts of the portfolio to restart the project

--- Juliana-portfolio/src/Header.jsx
+++ Juliana-portfolio/src/Header.jsx
@@ -1,18 +1,8 @@
 function Header() {
   return (
-    <div className="container mx-auto">
-      <header className="flex justify-between my-5">
-        <h1 className="logo-name">Juliana</h1>
-        <div className="nav-buttons-container">
-          <ul className="flex">
-            <li>About me</li>
-            <li>Process</li>
-            <li>Gallery</li>
-            <li>Testimonials</li>
-          </ul>
-        </div>
-      </header>
-    </div>
+    <section>
+      
+    </section>
   );
 }
 export default Header;
--- Juliana-portfolio/src/Hero.jsx
+++ Juliana-portfolio/src/Hero.jsx
@@ -1,23 +1,5 @@
 export default function Hero() {
   return (
-    <section className="container mx-auto">
-      <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
-        <div className="hero-img-container">
-          <img src="./src\assets\images\hero-img.svg" alt="" srcset="" />
-        </div>
-        <div className="greetings-container">
-          <p className="text-4xl sub-header-text mb-5">
-            Capturing Life in Watercolor
-          </p>
-          <p>
-            Hi, I’m Juliana, a watercolor artist inspired by nature, fleeting
-            moments, and the beauty of light and color. My work captures emotion
-            and wonder, transforming simple strokes into stories that connect
-            with the soul. Explore my portfolio and join me on this creative
-            journey.
-          </p>
-        </div>
-      </div>
-    </section>
+    <section></section>
   );
 }
