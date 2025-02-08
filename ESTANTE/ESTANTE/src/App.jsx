694a34901168bf25469aa942f90679320921d974
Joaquin Meroy <joaquinameroy@gmail.com>
2025-02-08 18:41:59 +0800

    added home

--- ESTANTE/ESTANTE/src/App.jsx
+++ ESTANTE/ESTANTE/src/App.jsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import SideNav from "./SideNave";
+import Home from "./Home";
 
 function App() {
   return (
-    <div>
+    <div className="flex">
       <SideNav></SideNav>
+      <div className="flex-1 p-10">
+      <Home></Home>
+      </div>
     </div>
   );
 }
