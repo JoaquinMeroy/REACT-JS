e61dc72b8da98a17e584a8304fc8daae35c571d0
Joaquin A. Meroy <joaquinameroy@gmail.com>
2025-02-13 17:04:04 +0800

    firebase
    refactor: added firebase to the project but still has errors.

--- ESTANTE/ESTANTE/src/App.jsx
+++ ESTANTE/ESTANTE/src/App.jsx
@@ -8,26 +8,42 @@ import UNILAB from "./UNILAB";
 import METRO from "./METRO";
 import GENERICS from "./GENERICS";
 import BRANDED from "./BRANDED";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "./firebase-config";
+import { useEffect } from "react";
 
 function App() {
+  const docRef = doc(db, "USERS", "SAfpMvZoRQh0oXBeT6UI");
+
+  const getData = async () => {
+    const docSnap = await getDoc(docRef);
+    console.log(docSnap);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <Router>
-      <div className="flex h-screen overflow-hidden">
-        <SideNav />
-        {/* Make sure the main content area does not overflow */}
-        <div className="flex-1 p-10 overflow-hidden">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/otc" element={<OTC />} />
-            <Route path="/zuelig" element={<ZUELIG />} />
-            <Route path="/unilab" element={<UNILAB />} />
-            <Route path="/metro" element={<METRO />} />
-            <Route path="/generics" element={<GENERICS />} />
-            <Route path="/branded" element={<BRANDED />} />
-          </Routes>
+    <div>
+      <Router>
+        <div className="flex h-screen overflow-hidden">
+          <SideNav />
+          {/* Make sure the main content area does not overflow */}
+          <div className="flex-1 p-10 overflow-hidden">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/otc" element={<OTC />} />
+              <Route path="/zuelig" element={<ZUELIG />} />
+              <Route path="/unilab" element={<UNILAB />} />
+              <Route path="/metro" element={<METRO />} />
+              <Route path="/generics" element={<GENERICS />} />
+              <Route path="/branded" element={<BRANDED />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 }
 
